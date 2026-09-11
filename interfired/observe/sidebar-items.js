e90c3bc83b0981ffd7234871daab7046b69c7871
Interchouette <484423+Interchouette@users.noreply.github.com>
2026-09-11 19:04:33 +0000

    Deploy documentation for cd90b79b8235537c433827eaf964700fd179275b

--- interfired/observe/sidebar-items.js
+++ interfired/observe/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["forced_ring_exit","handle_event","handle_event_for_live","run"]};
+window.SIDEBAR_ITEMS = {"fn":["forced_ring_exit","handle_event","handle_event_for_live","run","run_test_early"]};
